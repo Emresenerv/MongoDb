cb22caf38a163fbd5299af2d0e823848165a578f
AsusTEK <AsusTEK@DESKTOP-6JTJ49H>
2024-07-28 23:33:51 +0300

    Add project files.

---
+++ MongoDb/Services/ProductService/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using MongoDb.Dtos.ProductDtos;
+
+namespace MongoDb.Services.ProductService
+{
+    public interface IProductService
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task CreateProductAsync(CreateProductDto ProductDto);
+        Task UpdateProductAsync(UpdateProductDto ProductDto);
+        Task DeleteProductAsync(string id);
+        Task<GetByIdProductDto> GetByIdProductAsync(string id);
+    }
+}
